6fc243c7e9d5b164fcd466b075621057ded2cf61
Promise-nuwasa <promisenuwasasira@gmail.com>
2021-10-16 10:55:16 +0300

    Create a development

---
+++ app/src/main/java/com/example/fitapp/Slides.kt
@@ -0,0 +1,11 @@
+package com.example.fitapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Slides : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_slides)
+    }
+}
