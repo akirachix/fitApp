5b4de6d89f68b8ff9fbdae53d9d6a73d5e72a725
Promise-nuwasa <promisenuwasasira@gmail.com>
2021-10-22 09:14:20 +0300

    first six screens

--- app/src/main/java/com/example/fitapp/Slides.kt
+++ app/src/main/java/com/example/fitapp/Slides.kt
@@ -3,22 +3,31 @@ package com.example.fitapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.fitapp.databinding.ActivitySigninBinding
 import com.example.fitapp.databinding.ActivitySlidesBinding
 import com.example.fitapp.databinding.ActivitySplashBinding
 
 class Slides : AppCompatActivity() {
 
-    lateinit var binding: ActivitySlidesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding=DataBindingUtil.setContentView(
-            this, R.layout.activity_Slides )
+        setContentView(R.layout.activity_slides)
+
+        val binding: ActivitySlidesBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_slides
+        )
         // click listener for layout
-        binding.layoutSlides.setOnClickListener {
+
+        binding.btnaccount.setOnClickListener {
             val intent = Intent(this, Signup::class.java)
             startActivity(intent)
         }
+        binding.btnlogin.setOnClickListener {
+            val intent = Intent(this, Signin::class.java)
+            startActivity(intent)
 
+        }
     }
 }
