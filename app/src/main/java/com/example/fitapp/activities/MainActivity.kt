e861fe4c782e9e87156301e7bc985a19613565ff
Promise-nuwasa <promisenuwasasira@gmail.com>
2021-10-26 13:31:28 +0300

    added home screen

---
+++ app/src/main/java/com/example/fitapp/activities/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.fitapp.activities
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.fitapp.R
+import com.example.fitapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupViews()
+    }
+
+    fun setupViews() {
+        val bottomNavView : BottomNavigationView = binding.bottomNavView;
+        navController = Navigation.findNavController(this@MainActivity, R.id.fragNavHost)
+        NavigationUI.setupWithNavController(bottomNavView, navController)
+
+    }
+
+}
