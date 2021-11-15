fb57e5ed10b28b2574eed809f624b69b6fc3466c
Promise-nuwasa <promisenuwasasira@gmail.com>
2021-11-15 09:02:05 +0300

    Authentication

--- app/src/main/java/com/example/fitapp/viewModel/MpessaViewModel.kt
+++ app/src/main/java/com/example/fitapp/viewModel/MpessaViewModel.kt
@@ -1,22 +1,23 @@
-//package com.example.fitapp.viewModel
-//
-//import androidx.lifecycle.LiveData
-//import androidx.lifecycle.MutableLiveData
-//import androidx.lifecycle.ViewModel
-//import androidx.lifecycle.viewModelScope
-//import com.example.fitapp.Repositories.MpessaRepository
-//import dagger.hilt.android.lifecycle.HiltViewModel
-//import kotlinx.coroutines.launch
-//import javax.inject.Inject
-//
-//
-//@HiltViewModel
-//class MpessaViewModel @Inject constructor(var mpessaRepository: MpessaRepository): ViewModel() {
-//    lateinit var paymentLiveData: LiveData<List<MpessaActivity>>
-//    var errorLiveData = MutableLiveData<String>()
-//    fun fetchPayments(accessToken:String){
-//        viewModelScope.launch{
-//            mpessaRepository.fetchPayments((accessToken))
-//        }
-//    }
-//}
+package com.example.fitapp.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.fitapp.Repositories.MpessaRepository
+import com.example.fitapp.activities.PaymentActivity
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class MpessaViewModel @Inject constructor(var mpessaRepository: MpessaRepository): ViewModel() {
+    lateinit var paymentLiveData: LiveData<List<PaymentActivity>>
+    var errorLiveData = MutableLiveData<String>()
+    fun fetchPayments(accessToken:String){
+        viewModelScope.launch{
+            mpessaRepository.fetchPayments((accessToken))
+        }
+    }
+}
